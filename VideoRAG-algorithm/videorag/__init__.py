d34a63e3fd29ff1b1b43ed9124a257ae910051a7
CrudyLame <dopeyslimeg@gmail.com>
2025-10-20 19:52:43 +0300

    Refactor cloud pipeline into modular helpers

--- VideoRAG-algorithm/videorag/__init__.py
+++ VideoRAG-algorithm/videorag/__init__.py
@@ -1,1 +1,10 @@
-from .videorag import VideoRAG, QueryParam
+"""Public exports for the cloud-native VideoRAG package."""
+
+from .base import QueryParam
+from .videorag import CloudVideoRAGService, VideoRAG
+
+__all__ = [
+    "VideoRAG",
+    "QueryParam",
+    "CloudVideoRAGService",
+]
